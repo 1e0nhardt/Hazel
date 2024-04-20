f6ec877a3083c91c8bf824ff86f96f03690902a9
1e0nhardt <huwkigane@gmail.com>
2024-04-20 10:13:19 +0800

    增加spdlog日志子模块 使用premake5构建项目

--- Hazel/src/Hazel/EntryPoint.h
+++ Hazel/src/Hazel/EntryPoint.h
@@ -5,6 +5,10 @@ extern Hazel::Application* CreateApplication();
 
 int main(int argc, char** argv)
 {
+    Hazel::Log::Init();
+    HZ_CORE_WARN("Initailzed Log");
+    HZ_INFO("Hello!");
+
     auto app = CreateApplication();
     app->Run();
     delete app;
