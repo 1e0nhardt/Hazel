a64bfdd55263a34282ef2043a5ff03de543e0784
1e0nhardt <huwkigane@gmail.com>
2024-04-21 15:22:15 +0800

    一些整理工作

--- premake5.lua
+++ premake5.lua
@@ -11,14 +11,18 @@ IncludeDir["Glad"] = "Hazel/vendor/Glad/include"
 IncludeDir["ImGui"] = "Hazel/vendor/imgui"
 
 -- 将Hazel/vendor/GLFW目录下的premake5.lua包含过来
-include "Hazel/vendor/GLFW"
-include "Hazel/vendor/Glad"
-include "Hazel/vendor/imgui"
+group "Dependencies"
+	include "Hazel/vendor/GLFW"
+	include "Hazel/vendor/Glad"
+	include "Hazel/vendor/imgui"
+
+group ""
 
 project "Hazel"
     location "Hazel"
     kind "SharedLib"
     language "C++"
+	staticruntime "off"
 
     targetdir ("bin/" .. outputdir .. "/%{prj.name}")
     objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
@@ -67,17 +71,17 @@ project "Hazel"
 
     filter "configurations:Debug"
         defines "HZ_DEBUG"
-		buildoptions "/MDd"
+		runtime "Debug"
         symbols "On"
 
     filter "configurations:Release"
         defines "HZ_RELEASE"
-		buildoptions "/MD"
+		runtime "Release"
         optimize "On"
 
     filter "configurations:Dist"
         defines "HZ_DIST"
-		buildoptions "/MD"
+		runtime "Release"
         optimize "On"
 
 
@@ -85,6 +89,7 @@ project "Sandbox"
     location "Sandbox"
     kind "ConsoleApp"
     language "C++"
+	staticruntime "off"
 
     targetdir ("bin/" .. outputdir .. "/%{prj.name}")
     objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
@@ -106,7 +111,6 @@ project "Sandbox"
 
     filter "system:windows"
         cppdialect "C++17"
-        staticruntime "On"
         systemversion "latest"
 
         defines
@@ -117,15 +121,15 @@ project "Sandbox"
 
     filter "configurations:Debug"
         defines "HZ_DEBUG"
-		buildoptions "/MDd"
+		runtime "Debug"
         symbols "On"
 
     filter "configurations:Release"
         defines "HZ_RELEASE"
-		buildoptions "/MD"
+		runtime "Release"
         optimize "On"
 
     filter "configurations:Dist"
         defines "HZ_DIST"
-		buildoptions "/MD"
+		runtime "Release"
         optimize "On"
